41030605a5c387cf4daa5a73020975b229456488
Rama Reddy R <117635899+rrtechconnect@users.noreply.github.com>
2023-01-27 13:42:32 +0530

    Create 4_Leap_year.py

---
+++ Python_Professional_Bootcamp_2023/4_Leap_year.py
@@ -0,0 +1,7 @@
+year = int(input("Which year do you want to check? "))
+if year%4==0 and year%100!=0:
+    print ('Leap year.')
+elif year%4 == 0 and year%100 == 0 and year%400 == 0:
+    print('Leap year.')
+else:
+    print('Not leap year.')
