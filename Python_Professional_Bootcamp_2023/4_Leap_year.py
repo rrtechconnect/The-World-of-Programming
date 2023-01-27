fb3aded5ea5fdb596d93cb7f775bec33ea74c80e
Rama Reddy R <117635899+rrtechconnect@users.noreply.github.com>
2023-01-27 13:52:46 +0530

    Update 4_Leap_year.py

--- Python_Professional_Bootcamp_2023/4_Leap_year.py
+++ Python_Professional_Bootcamp_2023/4_Leap_year.py
@@ -6,16 +6,15 @@ elif year%4 == 0 and year%100 == 0 and year%400 == 0:
 else:
     print('Not leap year.')
 
+    
     #2 method
-
 if year % 4 == 0:
-    if year % 100 == 0;
-        if year % 400 == 0;
+    if year % 100 == 0:
+        if year % 400 == 0:
             print('Leap year')
         else:
-            print('Not leap year)
+            print('Not leap year')
     else:
-       print('Leap year')
+        print('Leap year')
 else:
-   print('Not leap year')
-             
+    print('Not leap year')
